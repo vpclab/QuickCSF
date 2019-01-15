5b3baf0628d0cd1f6b964becd357532416632693
Dominic Canare <dom@greenlightgo.org>
2019-01-15 15:23:40 -0600

    Add hotkey to minimize window

--- qCSF/__main__.py
+++ qCSF/__main__.py
@@ -314,6 +314,9 @@ class PeripheralCSFTester():
 					if 'g' in keys:
 						self.config['Gaze tracking']['show_gaze'] = not self.config['Gaze tracking']['show_gaze']
 
+					if 'm' in keys:
+						self.win.winHandle.minimize()
+
 					if 'space' in keys:
 						keepWaiting = False
 
