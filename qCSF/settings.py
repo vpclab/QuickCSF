bc4037ad1cfa7f6c4659d3ce745c135d1ab49a01
Dominic Canare <dom@greenlightgo.org>
2019-02-18 16:38:31 -0600

    Change ints to floats

--- qCSF/settings.py
+++ qCSF/settings.py
@@ -14,7 +14,7 @@ SETTINGS_GROUP = [
 
 	), ConfigGroup('Gaze tracking',
 		Setting('Wait for fixation',              bool,  False),
-		Setting('Max wait time (s)',              int,   10,   helpText='In seconds'),
+		Setting('Max wait time (s)',              float,   10,   helpText='In seconds'),
 		Setting('Gaze offset max (degrees)',      float, 1.5,  helpText='In degrees'),
 		Setting('Fixation period (seconds)',      float, 0.3,  helpText='In seconds'),
 		Setting('Render at gaze',                 bool,  False),
@@ -23,9 +23,9 @@ SETTINGS_GROUP = [
 		Setting('Show gaze',                      bool,  False),
 
 	), ConfigGroup('Display settings',
-		Setting('Monitor distance',   int,  57,        helpText='In cm'),
+		Setting('Monitor distance',   float,  57,        helpText='In cm'),
 		Setting('Background color',   str,  '#808080', helpText='Web-safe names or hex codes (#4f2cff)'),
-		Setting('Fixation size',      int,  20,        helpText='In arcmin'),
+		Setting('Fixation size',      float,  20,        helpText='In arcmin'),
 		Setting('Show fixation aid',  bool, False),
 		Setting('Fixation color',     str,  'black',   helpText='Web-safe names or hex codes (#4f2cff)'),
 		Setting('Show annuli',        bool, False),
@@ -33,14 +33,14 @@ SETTINGS_GROUP = [
 
 	), ConfigGroup('Stimuli settings',
 		# Practice settings
-		Setting('Eccentricities',               typing.List[int],        [4, 8, 12], helpText='In degrees'),
+		Setting('Eccentricities',               typing.List[float],        [4, 8, 12], helpText='In degrees'),
 		Setting('Orientations',                 typing.List[float],      [45, 67.5, 90, 112.5, 135], helpText='In degrees'),
-		Setting('Stimulus position angles',     typing.List[int],        [45, 135, 225, 315], helpText='In degrees'),
+		Setting('Stimulus position angles',     typing.List[float],        [45, 135, 225, 315], helpText='In degrees'),
 		Setting('Trials per stimulus config',   int,                     24),
 		Setting('Stimulus duration',            int,                     200, helpText='In ms'),
 		Setting('Time between stimuli',         int,                     1000, helpText='In ms'),
 		Setting('Contrast overrides',           typing.List[float],      []),
-		Setting('Stimulus size',                int,                     4, helpText='In degrees of visual angle'),
+		Setting('Stimulus size',                float,                     4, helpText='In degrees of visual angle'),
 		Setting('Mask time',                    int,                     0, helpText='In ms'),
 
 	), ConfigGroup('Input settings',
@@ -53,7 +53,4 @@ SETTINGS_GROUP = [
 ]
 
 def getSettings(filename = f'{PROGRAM_NAME}-settings.ini'):
-#	print(filename)
-#	if QApplication.instance() is None:
-#		_ = QApplication(['tmpApplication'])
 	return ConfigHelper(SETTINGS_GROUP, filename).getSettings()
