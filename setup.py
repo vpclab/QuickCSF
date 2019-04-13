0845ec3224790d221bff3f6292e94fb569b83f2a
Dominic Canare <dom@makeict.org>
2019-04-13 16:03:20 -0500

    Update meta info

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os import path
 from setuptools import setup
-
 import importlib
 
 dependencies = [ 'QtPy>=1.7.0', 'numpy>=1.16.2', 'matplotlib>=3.0.3', 'argparseqt>=0.2.1' ]
@@ -14,14 +14,24 @@ for binding in ['PySide2', 'PyQt5', 'PySide', 'PyQt']:
 else:
 	dependencies.append('PySide2>=5.12.2')
 
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
 setup(
 	name='QuickCSF',
 	version='2.0',
-	description='A fast, adaptive approach to estimating contrast sensitivity function parameters across multiple eccentricities and stimulus orientations.',
+	description='A fast, adaptive approach to estimating contrast sensitivity function parameters',
+	url='https://github.com/domstoppable/QuickCSF',
 	author='Dominic Canare',
 	author_email='dom@dominiccanare.com',
+	license='GPL',
 	packages=['QuickCSF'],
 	install_requires=dependencies,
+	long_description=long_description,
+	long_description_content_type='text/markdown',
 	package_data={'QuickCSF': ['assets/*']},
 	include_package_data=True
 )
